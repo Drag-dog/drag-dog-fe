f2549ff2a41afefbbb7c8b255b3968369b80c3e4
Jchan <gkswocks123@gmail.com>
2023-06-28 10:03:11 +0900

    ✨ Feat : mui init

---
+++ src/components/providers/MuiThemeProvider/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider, createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { configMuiBreakpoints } from "../../../constants/config/configMuiBreakpoints";
+import { configMuiComponents } from "../../../constants/config/configMuiComponents";
+import { configMuiPalette } from "../../../constants/config/configMuiPalette";
+import { configMuiTypography } from "../../../constants/config/configMuiTypography";
+
+let theme = createTheme({
+  typography: { ...configMuiTypography },
+  palette: { ...configMuiPalette },
+  breakpoints: { ...configMuiBreakpoints },
+  components: { ...configMuiComponents },
+});
+
+theme = responsiveFontSizes(theme);
+
+export const MuiThemeProvider = ({ children }: { children: React.ReactNode }) => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    {children}
+  </ThemeProvider>
+);
---
+++ src/constants/config/configMuiBreakpoints.ts
@@ -0,0 +1,8 @@
+import { BreakpointsOptions } from "@mui/material";
+import { breakPoints } from "../breakPoints";
+
+export const configMuiBreakpoints: BreakpointsOptions = {
+  values: {
+    ...breakPoints,
+  },
+};
