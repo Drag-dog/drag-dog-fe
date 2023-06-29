c32f7606a7439daecee52cbad1d3bc497682c7b6
Jchan <gkswocks123@gmail.com>
2023-06-29 12:12:41 +0900

    ✨ Feat : Loading 컴포넌트 추가

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -4,3 +4,4 @@ export { MainCtaBtn } from "./MainCtaBtn";
 export { PwField } from "./PwField";
 export { TextFieldControlled } from "./TextFieldControlled";
 export { Title } from "./Title";
+export { Loading } from "./Loading";
