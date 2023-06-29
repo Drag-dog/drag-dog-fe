5bdd94f90f42daa078d47437f571567d585b16cf
Jchan <gkswocks123@gmail.com>
2023-06-29 12:26:21 +0900

    ✨ Feat : react qurty config 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { AppRoutes } from "./components/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { JotaiProvider, MuiThemeProvider } from "./components/providers";
+import { ReactQueryProvider } from "./components/providers/ReactQueryProvider";
 
 function App() {
   return (
     <>
       <Router>
         <JotaiProvider>
-          <MuiThemeProvider>
-            <AppRoutes />
-          </MuiThemeProvider>
+          <ReactQueryProvider>
+            <MuiThemeProvider>
+              <AppRoutes />
+            </MuiThemeProvider>
+          </ReactQueryProvider>
         </JotaiProvider>
       </Router>
     </>
---
+++ src/components/providers/ReactQueryProvider.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+export const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
