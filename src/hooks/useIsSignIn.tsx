25bba50263d69c8cc836516a5ee39014add9262b
Jchan <gkswocks123@gmail.com>
2023-08-26 14:21:37 +0900

    Feat : useSetLoginState 추가 - MainLayout 컴포넌트에서 사용

--- src/hooks/useIsSignIn.tsx
+++ src/hooks/useIsSignIn.tsx
@@ -1,37 +1,10 @@
-import React from "react";
-import { accessTokenAtom } from "../store/atoms";
+import { loginStateAtom } from "../store/atoms";
 import { useAtomValue } from "jotai";
-import { userApi } from "../apis/user";
-import { LOGIN_STATE } from "../constants/enum";
 
 /**
  * @description 로그인 상태를 확인하는 커스텀 훅
  */
 export const useIsSignIn = () => {
-  const accessToken = useAtomValue(accessTokenAtom);
-  const [loginState, setLoginState] = React.useState<LOGIN_STATE>(LOGIN_STATE.UNKNOWN);
-
-  React.useLayoutEffect(() => {
-    if (accessToken === "") {
-      setLoginState(LOGIN_STATE.UNKNOWN);
-    } else {
-      userApi
-        .getIsSignIn(accessToken)
-        .then(({ data }) => {
-          switch (data?.isLoggedIn) {
-            case true:
-              setLoginState(LOGIN_STATE.LOGGED_IN);
-              break;
-            case false:
-            default:
-              setLoginState(LOGIN_STATE.NOT_LOGGED_IN);
-              break;
-          }
-        })
-        .catch(() => {
-          setLoginState(LOGIN_STATE.UNKNOWN);
-        });
-    }
-  }, [accessToken]);
+  const loginState = useAtomValue(loginStateAtom);
   return { loginState };
 };
---
+++ src/hooks/useSetLoginState.ts
@@ -0,0 +1,42 @@
+import React from "react";
+import { useMutation } from "react-query";
+import { useAtomValue, useSetAtom } from "jotai";
+import { accessTokenAtom, loginStateAtom } from "../store/atoms";
+import { userApi } from "../apis/user";
+import { LOGIN_STATE } from "../constants/enum";
+
+/**
+ * @description 엑세스 토큰을 이용해 로그안 상태를 설정하는 커스텀 훅
+ * MainLayout 컴포넌트에서 사용
+ */
+export const useSetLoginState = () => {
+  const accessToken = useAtomValue(accessTokenAtom);
+  const setAccessToken = useSetAtom(accessTokenAtom);
+  const setLoginState = useSetAtom(loginStateAtom);
+  const mutGetIsSignIn = useMutation({
+    mutationFn: async () => {
+      const res = await userApi.getIsSignIn(accessToken);
+      return res.data;
+    },
+    onSuccess: (data) => {
+      switch (data?.isLoggedIn) {
+        case true:
+          setLoginState(LOGIN_STATE.LOGGED_IN);
+          break;
+        case false:
+        default:
+          setLoginState(LOGIN_STATE.NOT_LOGGED_IN);
+          break;
+      }
+    },
+    onError: () => {
+      setLoginState(LOGIN_STATE.UNKNOWN);
+      setAccessToken("");
+    },
+  });
+
+  React.useLayoutEffect(() => {
+    if (accessToken !== "") mutGetIsSignIn.mutate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
